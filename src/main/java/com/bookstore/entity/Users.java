4f27071a0f217bb9ee62e25f2cba94294d110234
DemonVetarian <klintonpcb@gmail.com>
2022-03-06 01:01:08 +0530

    Commit 05/03/2022

---
+++ src/main/java/com/bookstore/entity/Users.java
@@ -0,0 +1,49 @@
+package com.bookstore.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+	
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userid;
+	private String email;
+	private String password;
+	@Column(name = "full_name")
+	private String fullName;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
 <html>
+<head>
+<meta charset="ISO-8859-1">
+<title>BookStore Website</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h1>Welcome to bookstore website</h1>
 </body>
 </html>
